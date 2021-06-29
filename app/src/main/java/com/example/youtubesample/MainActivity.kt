07d669713096f38dec79817222469ba03c8412d0
한경동 <hkd0694@gmail.com>
2021-06-30 00:21:37 +0900

    유튜브 샘플 예제 완성

--- app/src/main/java/com/example/youtubesample/MainActivity.kt
+++ app/src/main/java/com/example/youtubesample/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.youtubesample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : YouTubeBaseActivity() {
+
+    private var yListener: YouTubePlayer.OnInitializedListener?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_click.setOnClickListener {
+            ytv_player.initialize("develop", object : YouTubePlayer.OnInitializedListener {
+                override fun onInitializationSuccess( provider: YouTubePlayer.Provider, player: YouTubePlayer, wasRestored: Boolean ) {
+                    if (!wasRestored) {
+                        player.cueVideo("F-KjYNmsi0U")
+                    }
+                }
+                override fun onInitializationFailure( provider: YouTubePlayer.Provider?, result: YouTubeInitializationResult? ) {
+
+                }
+            })
+        }
     }
 }
